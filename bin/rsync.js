0422b260c92138b0fc0c18efeaeef7b96713aa98
Peter Ferrie <peter.ferrie@gmail.com>
2019-01-15 21:50:51 -0800

    sync

---
+++ bin/rsync.js
@@ -0,0 +1,19 @@
+a = new ActiveXObject("scripting.filesystemobject")
+b = WScript.Arguments(0).lastIndexOf('\\') + 1
+c = WScript.Arguments(0).substr(0, b)
+d = WScript.Arguments(0).substr(b)
+if (d.substr(0, 2) == '*.')
+{
+    d = d.substr(2)
+}
+for (b = new Enumerator(a.GetFolder(c).files); !b.atEnd(); b.moveNext())
+{
+    if ((d == '*') || (d == a.getextensionname(b.item().name)))
+    {
+        e = WScript.Arguments(1) + "\\" + b.item().Name
+        if (!a.fileexists(e) || (a.getfile(e).datelastmodified < b.item().datelastmodified))
+        {
+            new ActiveXObject("wscript.shell").run('cmd /c copy "' + c + b.item().name + '" ' + WScript.Arguments(1), 0, 1)
+        }
+    }
+}
