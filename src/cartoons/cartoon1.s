a2e269ab3b6770cd07fef3e713f268c9a8f5e267
4am <a2_4am@sent.com>
2019-06-28 20:59:46 -0400

    Spare Change cartoons

---
+++ src/cartoons/cartoon1.s
@@ -0,0 +1,52 @@
+!cpu 65c02
+!to "cartoon.1#0602d0",plain
+*=$2d0
+
+cartoon=1
+
+load=$2700
+buffer=$2000
+
+	jsr	$bf00
+	!byte	$c8		;open file
+	!word	c8_parms	;->bd5d
+;	lda	c8_parms+5
+;	sta	ca_parms+1
+	jsr	$bf00
+	!byte	$ca		;read file
+	!word	ca_parms
+	jsr	$bf00
+	!byte	$cc		;close
+	!word	cc_parms
+lda #>callback
+sta $277a
+lda #<callback-1
+sta $277d
+jmp $2700
+callback:
+lda #cartoon-1
+sta $19c7
+lda #$4c
+sta $1832
+jsr $2000
+jmp $100
+
+
+c8_parms			;bd5d
+	!byte	3
+	!word	filename
+	!word	buffer		;somewhere
+	!byte	0
+
+ca_parms			;bd68
+	!byte	4
+cc_parms			;bd70
+	!byte	1
+	!word	$2700
+	!word	$ffff
+	!word	$ffff
+
+filename
+	!byte	(filename_e-filename)-1
+	!text	"SPARE.CHANGE"
+filename_e
