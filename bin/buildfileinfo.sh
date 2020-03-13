6b8c7044594e0bd2e6a68615f8f16c745acbb325
4am <a2_4am@sent.com>
2020-03-12 20:56:36 -0400

    .

--- bin/buildfileinfo.sh
+++ bin/buildfileinfo.sh
@@ -4,7 +4,7 @@ indir=$1
 intype=$2
 inaddress=$3
 cd "$indir"
-touch _FileInformation.txt
+rm -f _FileInformation.txt
 (for f in *; do
     echo "$f=Type($intype),AuxType($inaddress),Access(C3)"
- done) >> _FileInformation.txt
+ done) > _FileInformation.txt
