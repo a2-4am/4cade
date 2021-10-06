cbc0f4191a6074f80cb0aa6b658723cf52a06561
4am <a2_4am@sent.com>
2021-10-06 00:40:16 -0400

    tidy up Bash script

--- bin/buildhelp.sh
+++ bin/buildhelp.sh
@@ -2,16 +2,15 @@
 
 # run from project root directory
 
-games=`cat res/GAMES.CONF | grep "," | grep -v "^#" | cut -d"," -f2 | cut -d"=" -f1 | sort`
-cat res/GAMEHELP/STANDARD > "$1"
+games=$(grep "," res/GAMES.CONF | grep -v "^#" | cut -d"," -f2 | cut -d"=" -f1 | sort)
+cp res/GAMEHELP/STANDARD "$1"
 for c in {A..Z}; do
-    echo -e "group$c"
-    echo "$games" | grep "^$c" | while read game; do
+    echo "group$c"
+    echo "$games" | grep "^$c" | while read -r game; do
         echo "!byte ${#game}"
         echo "!text \"$game\""
         if [ -f "res/GAMEHELP/$game" ]; then
-            offset=$(wc -c <"$1")
-            echo "!be24 $offset"
+            echo "!be24 $(wc -c <"$1")"
             cat res/GAMEHELP/"$game" >> "$1"
         else
             echo "!be24 0"
