cf9c57d2425b56e19fb28a0450eccd6083d9056c
4am <a2_4am@sent.com>
2021-10-06 00:54:17 -0400

    tidy Bash script

--- bin/buildhelp.sh
+++ bin/buildhelp.sh
@@ -6,7 +6,7 @@ games=$(grep "," res/GAMES.CONF | grep -v "^#" | cut -d"," -f2 | cut -d"=" -f1 |
 cp res/GAMEHELP/STANDARD "$1"
 for c in {A..Z}; do
     echo "group$c"
-    echo "$games" | grep "^$c" | while read -r game; do
+    for game in $(echo "$games" | grep "^$c"); do
         echo "!byte ${#game}"
         echo "!text \"$game\""
         if [ -f "res/GAMEHELP/$game" ]; then
