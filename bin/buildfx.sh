1d3f612c790c8c4b8794a65b6a17ec747e0fc61f
4am <a2_4am@sent.com>
2021-10-08 12:20:42 -0400

    some comments

--- bin/buildfx.sh
+++ bin/buildfx.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+# create or truncate merged-effects file
 :>| "$3"
+
+# make temp file with list of effect names
 records=$(mktemp)
 grep -v "^$" < "$1" | grep -v "^#" | grep -v "^\[" > "$records"
+
+# make temp assembly source file that represents the binary OKVS data structure
 source=$(mktemp)
-(echo "*=0"
- echo "!le16 $(wc -l <"$records"), 0"
+(echo "*=0"                            # dummy program counter for assembler
+ echo "!le16 $(wc -l <"$records"), 0"  # OKVS header
  while read -r key; do
-     echo "!byte ${#key}+5"
-     echo "!byte ${#key}"
-     echo "!text \"$key\""
-     echo "!be24 $(wc -c <"$3")"
-     cat "build/FX/$key" >> "$3"
+     echo "!byte ${#key}+5"            # OKVS record length
+     echo "!byte ${#key}"              # OKVS key length
+     echo "!text \"$key\""             # OKVS key (effect name)
+     echo "!be24 $(wc -c <"$3")"       # offset into merged-effects file
+     cat "build/FX/$key" >> "$3"       # add effect code into merged-effects file
+                                       # (all effects were previously assembled)
  done < "$records") > "$source"
+
+# assemble temp source file to create binary OKVS data structure
 acme -o "$2" "$source"
+
+# clean up
 rm "$source"
 rm "$records"
--- bin/buildokvs.sh
+++ bin/buildokvs.sh
@@ -2,18 +2,25 @@
 
 # run from project root directory
 
+# make temp file with just the key/value pairs (strip blank lines, comments, eof marker)
 records=$(mktemp)
 grep -v "^$" < "$1" | grep -v "^#" | grep -v "^\[" > "$records"
+
+# make temp assembly source file that represents the binary OKVS data structure
 source=$(mktemp)
-(echo "*=0"
- echo "!le16 $(wc -l <"$records"), 0"
+(echo "*=0"                            # dummy program counter for assembler
+ echo "!le16 $(wc -l <"$records"), 0"  # OKVS header
  while IFS="=" read -r key value; do
-     echo "!byte ${#key}+${#value}+3"
-     echo "!byte ${#key}"
-     echo "!text \"$key\""
-     echo "!byte ${#value}"
-     echo "!text \"$value\""
+     echo "!byte ${#key}+${#value}+3"  # OKVS record length
+     echo "!byte ${#key}"              # OKVS key length
+     echo "!text \"$key\""             # OKVS key
+     echo "!byte ${#value}"            # OKVS value length
+     echo "!text \"$value\""           # OKVS value
  done < "$records") > "$source"
+
+# assemble temp source file to create binary OKVS data structure
 acme -o "$2" "$source"
+
+# clean up
 rm "$source"
 rm "$records"
