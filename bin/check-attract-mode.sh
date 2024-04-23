43ab54a12069df711c1fccd91facf72589c2c980
4am <a2_4am@sent.com>
2024-04-22 21:58:09 -0400

    add Yewdow

--- bin/check-attract-mode.sh
+++ bin/check-attract-mode.sh
@@ -50,13 +50,13 @@ cat res/GAMES.CONF |
 grep '^\!to' src/demo/*.a | cut -d'/' -f5-|cut -d'#' -f1 |
     grep -v "SPCARTOON" |
     while read f; do
-        grep "$f=0" res/ATTRACT.CONF >/dev/null || echo "unused demo: $f";
+        grep "^$f=0" res/ATTRACT.CONF >/dev/null || echo "unused demo: $f";
     done
 
 # warn about unused slideshows
 cd res/SS
 for f in *.CONF; do
-    grep "$f" ../ATTRACT.CONF >/dev/null || echo "unused slideshow: $f";
+    grep "^$f=" ../ATTRACT.CONF >/dev/null || echo "unused slideshow: $f";
 done
 cd ../..
 
